96b8e3a9533af0c23fd579f0abd0b6cd83cf7c5e
SamihEisa <sameh_eisa@hotmail.com>
2021-05-17 17:31:29 +0100

    claim, endorsement , verification test cases - python

--- python-marshaller/test/test_loc_claim.py
+++ python-marshaller/test/test_loc_claim.py
@@ -8,24 +8,86 @@
 
 import unittest
 from surethingcore.loc_claim_pb2 import LocationClaim
+from surethingcore.wi_fi_pb2 import WiFiNetworksEvidence
+from google.protobuf.any_pb2 import Any
+from google.protobuf.timestamp_pb2 import Timestamp
+import datetime
+
 
 class TestLocationClaim(unittest.TestCase):
 
     def test_location_claim(self):
         # test creation of a location claim
 
+        #### create a claim
+        claim = LocationClaim()
 
-        # create a claim
+        # claimId
+        claim.claimId = "1"
 
-        # serialize the claim
+        # proverId
+        claim.proverId = "1"
+
+        # location - oneof
+        claim.location.latLng.latitude =  56.3
+        claim.location.latLng.longitude = 56.3
+
+        #time - oneof
+        now = datetime.datetime.now()
+        claim.time.timestamp.FromDatetime(now)
+
+        # evidence type
+        claim.evidenceType ="eu.surething_project.core.wi_fi.WiFiNetworksEvidence"
+
+        #evidence - Any
+        wifi_evidence = WiFiNetworksEvidence()
+        wifi_evidence.id = "ABC"
+        ap = wifi_evidence.aps.add()
+        ap.ssid = "ssid-A"
+        ap.rssi = "-89"
+
+        evidence_any = Any()
+        evidence_any.Pack(wifi_evidence)   # pack Any
+
+        claim.evidence.CopyFrom(evidence_any)
+
+        #### serialize the claim
+        claim_serialized = claim.SerializeToString()
+
+        #### deserialize the claim
+        claim_deserialized = LocationClaim()
+        claim_deserialized.ParseFromString(claim_serialized)
 
 
-        # deserialize the claim
+        evidence = Any()
+        evidence.CopyFrom(claim_deserialized.evidence)
+        wifi_evidence = WiFiNetworksEvidence()
+        evidence.Unpack(wifi_evidence) # unpack Any
+        ap = wifi_evidence.aps[0]
 
+        timestamp = Timestamp()
+        timestamp.FromDatetime(now)
+        seconds = timestamp.seconds
+        nanos = timestamp.nanos
 
-        # assert all fields
-
-        pass
+        #### assert all fields
+        assert(claim_deserialized.claimId,
+               claim_deserialized.proverId,
+               claim_deserialized.location.latLng.latitude,
+               claim_deserialized.location.latLng.longitude,
+               claim_deserialized.time.timestamp.seconds,
+               claim_deserialized.time.timestamp.nanos,
+               claim_deserialized.evidenceType,
+               wifi_evidence.id,
+               ap.ssid,
+               ap.rssi
+               ) == ("1", "1",
+                     56.3, 56.3,
+                     seconds,
+                     nanos,
+                     "eu.surething_project.core.wi_fi.WiFiNetworksEvidence",
+                     "ABC", "ssid-A", "-89"
+                     )
 
 if __name__ == '__main__':
     unittest.main()
--- python-marshaller/test/test_loc_endorse.py
+++ python-marshaller/test/test_loc_endorse.py
@@ -7,22 +7,78 @@
 '''
 
 import unittest
-import surethingcore
+from surethingcore.loc_endorse_pb2 import LocationEndorsement
+from surethingcore.wi_fi_pb2 import WiFiNetworksEvidence
+from google.protobuf.any_pb2 import Any
+from google.protobuf.timestamp_pb2 import Timestamp
+import datetime
 
 class TestLocationEndorsement(unittest.TestCase):
 
     def test_location_endorsement(self):
         # test creation of a location endorsement
 
-        # create an endorsement
+        #### create an endorsement
+        endorsement = LocationEndorsement()
 
-        # serialize the endorsement
+        # witnessId
+        endorsement.witnessId = "1"
 
-        # deserialize the endorsement
+        # claimId
+        endorsement.claimId = "1"
 
-        # assert all fields
+        # time - oneof
+        now = datetime.datetime.now()
+        endorsement.time.timestamp.FromDatetime(now)
 
-        pass
+        # evidence type
+        endorsement.evidenceType = "eu.surething_project.core.wi_fi.WiFiNetworksEvidence"
+
+        # evidence - Any
+        wifi_evidence = WiFiNetworksEvidence()
+        wifi_evidence.id = "DEF"
+        ap = wifi_evidence.aps.add()
+        ap.ssid = "ssid-B"
+        ap.rssi = "-90"
+
+        evidence_any = Any()
+        evidence_any.Pack(wifi_evidence)  # pack Any
+
+        endorsement.evidence.CopyFrom(evidence_any)
+
+        #### serialize the endorsement
+        endorsement_serialized = endorsement.SerializeToString()
+
+        #### deserialize the endorsement
+        endorsement_deserialized = LocationEndorsement()
+        endorsement_deserialized.ParseFromString(endorsement_serialized)
+
+        evidence = Any()
+        evidence.CopyFrom(endorsement_deserialized.evidence)
+        wifi_evidence = WiFiNetworksEvidence()
+        evidence.Unpack(wifi_evidence)  # unpack Any
+        ap = wifi_evidence.aps[0]
+
+        timestamp = Timestamp()
+        timestamp.FromDatetime(now)
+        seconds = timestamp.seconds
+        nanos = timestamp.nanos
+
+        #### assert all fields
+        assert (endorsement_deserialized.witnessId,
+                endorsement_deserialized.claimId,
+                endorsement_deserialized.time.timestamp.seconds,
+                endorsement_deserialized.time.timestamp.nanos,
+                endorsement_deserialized.evidenceType,
+                wifi_evidence.id,
+                ap.ssid,
+                ap.rssi
+                ) == ("1", "1",
+                      seconds,
+                      nanos,
+                      "eu.surething_project.core.wi_fi.WiFiNetworksEvidence",
+                      "DEF", "ssid-B", "-90"
+                      )
 
 if __name__ == '__main__':
     unittest.main()
